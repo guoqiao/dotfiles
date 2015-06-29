7476eb592501d42b30fac86e428992804334b9c1
Joe Guo <joeg@catalyst.net.nz>
2015-06-30 10:51:56 +1200

    add ack-grep

--- ubuntu/apt-get.sh
+++ ubuntu/apt-get.sh
@@ -1,4 +1,4 @@
 sudo apt-get update
 sudo apt-get install -y \
-    git vim zsh tmux wget curl htop \
+    git vim zsh tmux wget curl htop ack-grep \
     build-essential python-setuptools python-dev libmysqlclient-dev python-mysqldb
